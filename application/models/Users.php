64bfb1a1b8e12874155a9442a0880ad667588c69
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-03-16 15:20:50 +0200

    registeration and loginform

--- application/models/Users.php
+++ application/models/Users.php
@@ -4,5 +4,19 @@ class Application_Model_Users extends Zend_Db_Table_Abstract
 {
     protected $_name = "users";
 
+ 	function Register($formData)
+	{
+
+	$row=$this->createRow();
+	$row->userName=$formData['userName'];
+	$row->email=$formData['email'];
+	$row->password=$formData['password'];
+	$row->save();
+
+	}
+
+
+
+
 }
 
