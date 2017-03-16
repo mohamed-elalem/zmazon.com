4d4e1ea322294534af81f0b5891579e6a90b23b5
mohamed <muhammed_magdy838@yahoo.com>
2017-03-16 16:48:48 +0200

    Magdy

---
+++ application/controllers/CustomerUserController.php
@@ -0,0 +1,56 @@
+<?php
+
+class CustomerUserController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+    /*
+     * Author : Magdy
+     */
+
+    public function putRateAction()
+    {
+        // action body
+    }
+    /*
+     * Author : Magdy
+     */
+    public function addToWishListAction()
+    {
+        // action body
+    }
+    /*
+     * Author : Magdy
+     */
+    public function addToShoppingCartAction()
+    {
+        // action body
+    }
+    /*
+     * Author: Magdy
+     */
+
+    public function commentAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
