3163e9f59475566da740d2b9e90b9dcc8c749267
Mohamed El-Alem <mohamed.el.alem.2017@gmail.com>
2017-03-16 16:18:24 +0200

    Adding admin update user info

--- application/models/Users.php
+++ application/models/Users.php
@@ -3,6 +3,18 @@
 class Application_Model_Users extends Zend_Db_Table_Abstract
 {
     protected $_name = "users";
+    
+    public function retrieveAllUsers() {
+        return $this->fetchAll();
+    }
+    
+    public function editRecord($id, $data) {
+        $this->update($data, "id = ".$id);
+    }
+    
+    public function remove($id) {
+        $this->delete("id = ".$id);
+    }
 
 }
 
