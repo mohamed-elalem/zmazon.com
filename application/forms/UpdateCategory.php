d21fe5cddacb84dcd11e71d3f979a18b88cfb66e
Mohamed El-Alem <mohamed.el.alem.2017@gmail.com>
2017-03-16 21:33:48 +0200

    Finishing crud on users/categories

--- application/forms/UpdateCategory.php
+++ application/forms/UpdateCategory.php
@@ -5,7 +5,19 @@ class Application_Form_UpdateCategory extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $category = new Zend_Form_Element_Text("category");
+        $category->setAttribs(array(
+            "class" => 'form-control',
+            'placeholder' => 'e.g. Sports'
+        ));
+        $category->setLabel("New category name");
+        $category->setRequired();
+        $category->addValidator("StringLength", false, array(4,100));
+        
+        $submit = new Zend_Form_Element_Submit("update");
+        $submit->setAttrib("class", "btn btn-primary");
+        
+        $this->addElements(array($category, $submit));
     }
 
 
