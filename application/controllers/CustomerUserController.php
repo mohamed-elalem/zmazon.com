69e776d914ea35da0db795efbf34e2261536460d
mohamed <muhammed_magdy838@yahoo.com>
2017-03-17 04:32:24 +0200

    Ajax trial success in the main index page

--- application/controllers/CustomerUserController.php
+++ application/controllers/CustomerUserController.php
@@ -20,7 +20,12 @@ class CustomerUserController extends Zend_Controller_Action
 
     public function addToWishListAction()
     {
-        // action body
+        
+
+        $wishList = new Application_Model_WishList();
+        $user_id  = $this->_request->getParam('user_id');
+        $product_id  = $this->_request->getParam('product_id');
+        $wishList->add($user_id, $product_id);
     }
 
     public function addToCartAction()
--- application/models/WishList.php
+++ application/models/WishList.php
@@ -3,6 +3,18 @@
 class Application_Model_WishList extends Zend_Db_Table_Abstract
 {
     protected $_name  = "wishList";
+    
+    public function add($user_id, $product_id){
+       $row=$this->createRow();
+       $row->userId = $user_id;
+       $row->productId = $product_id;
+       $row->save();
+       
+        
+    }
+    public function delete($user_id, $product_id){
+        
+    }
 
 }
 
